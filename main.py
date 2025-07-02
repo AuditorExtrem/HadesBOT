d157b7e042001f45429406b18fe80ec5ea7aca47
AuditorBullet <agentelobi@gmail.com>
2025-07-02 00:19:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ async def enviar_aviso(tipo, canal_id, cargo_id, texto_padrao):
         embed = discord.Embed(
             title=dados.get("titulo", "⏳ Guild Donation Coming Up"),
             description=dados.get("descricao", "Prepare your donations in advance!"),
-            color=discord.Color.orange()
+            color=discord.Color.Blue()
         )
         if dados.get("imagem"):
             embed.set_image(url=dados["imagem"])
